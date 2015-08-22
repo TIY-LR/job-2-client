c51850206b366b71b6f8150294fb48331953e207
Corbin Otwell <cbotwell@gmail.com>
2015-08-21 23:04:40 -0500

    adds basic lead form for creating leads

---
+++ app/components/lead-form/component.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  classNames: ['container'],
+  actions: {
+    submitLead: function(model) {
+      // send to route, get properties
+    },
+  },
+});
--- app/lead/create/route.js
+++ app/lead/create/route.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    submitLead: function(model) {
+      //takes model and sends create request to server
+    }
+  }
 });
