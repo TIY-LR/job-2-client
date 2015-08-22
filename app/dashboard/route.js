3bb2433f6249ecb7b8179cbb7d516a8a5c967321
Corbin Otwell <cbotwell@gmail.com>
2015-08-22 09:15:05 -0500

    adds adapter and console.logs test ajax data via server

--- app/dashboard/route.js
+++ app/dashboard/route.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() {
+    return Ember.$.ajax('http://jobhunta.azurewebsites.net/api/values');
+  },
+
+  afterModel: function() {
+    console.log(this.model());
+  }
 });
